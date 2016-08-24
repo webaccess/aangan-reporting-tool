eea6d885e78be4710be5296a65c69f75eb8cdd93
ranvijay <ranvijay5686@gmail.com>
2016-08-24 13:14:47 +0530

    database models added

---
+++ aangan_reporting_tool/aangan_report/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class AanganReportConfig(AppConfig):
+    name = 'aangan_report'
---
+++ aangan_reporting_tool/aangan_report/models.py
@@ -0,0 +1,69 @@
+from __future__ import unicode_literals
+
+from datetime import datetime
+from model_utils.models import TimeStampedModel
+
+from django.db import models
+
+
+class ScoringSchema(TimeStampedModel):
+    name = models.CharField(max_length=200, verbose_name ='Name')
+    color = models.CharField(max_length=50, verbose_name='Color')
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        db_table = "scoring_schema"
+
+
+class Category(TimeStampedModel):
+    name = models.CharField(max_length=300, verbose_name ='Category Name')
+    description = models.TextField()
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        db_table = "category"
+
+
+class Question(TimeStampedModel):
+    category = models.ForeignKey(Category, verbose_name ='Category Id')
+    name = models.CharField(max_length=200, verbose_name ='Question Name')
+    formula = models.TextField()
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        db_table = "question"
+
+
+class ReportInstance(TimeStampedModel):
+    doj = models.DateTimeField(default=datetime.now, verbose_name ='Date Of Joining')
+
+    class Meta:
+        db_table = "report_instance"
+
+
+class QuestionInstance(TimeStampedModel):
+    report_instance = models.ForeignKey(ReportInstance, verbose_name='Report Instance Id')
+    question = models.ForeignKey(Question, verbose_name='Question Id')
+    value = models.CharField(max_length=200, verbose_name='Value')
+
+    class Meta:
+        db_table = "question_instance"
+
+
+class CategoryInstance(TimeStampedModel):
+    report_instance = models.ForeignKey(ReportInstance, verbose_name='Report Instance Id')
+    category = models.ForeignKey(Category, verbose_name='Category Id')
+    value = models.CharField(max_length=200, verbose_name='Value')
+    sugestion_text = models.TextField()
+
+    class Meta:
+        db_table = "category_instance"
+
+
+
